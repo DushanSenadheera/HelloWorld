5a064edcfb9df782f90e8eaf87e1fe9c1395ecd2
Ashan Dimantha <85834813+ashandimantha2000@users.noreply.github.com>
2023-02-24 13:32:27 +0530

    Add and Update of Inventry

---
+++ UpdatePharmacyDashboardInventry.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/parmacyDashboardProfile.css">
+    <title>Tabsule Dashboard</title>
+</head>
+
+<body>
+    <nav>
+        <h3>LOGO</h3>
+        <h5>Tabsule</h5>
+    </nav>
+    <div class="navSection">
+        <div class="navlinks">
+            <ul>
+                <li><a href="">Inventory</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="">Logout</a></li>
+            </ul>
+        </div>
+        <div class="navContent">
+            <h4>Inventry/Add Drug</h4>
+            <br>
+            <form action="" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <select class="drugs-drop" name="drug" id="drug">
+                                <option value="Drug1">Drug 1</option>
+                                <option value="Drug2">Drug 2</option>
+                                <option value="Drug3">Drug 3</option>
+                                <option value="Drug4">Drug 4</option>
+                            </select>
+                        </td>
+                        <td><input type="text" placeholder="Manufacture" name="manufacturer"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="tel" placeholder="Supplier" name="supplier"></td>
+                        <td><input type="email" placeholder="NDC (National Drug Cable)" name="ndc"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="date" class="inventry-data" placeholder="Select Expiration Date"
+                                name="expiration"></td>
+                        <td><input type="number" name="quantityonhand" placeholder="Quantity on hand" min="0"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" placeholder="Unit Price"></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Update Drug" class="save-btn">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
--- signin.php
+++ signin.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/signIn.css">
-    <title>Home Pharmacy</title>
+    <title>SignIn with Tabsule</title>
 </head>
 <body>
 <body>
@@ -13,7 +13,7 @@
         <div class="sign-in">
             <div class="sign-in-form">
                 <h1>Sign In</h1>
-                <form action="index.php" method="POST">
+                <form action="backend/usersignin.php" method="POST">
                     <input type="text" name="email" placeholder="Email" required>
                     <br>
                     <input type="password" name="password" placeholder="Password" required>
