2cdb17f1c351ece5e30c6d9612ba267ec916a90a
Ashan Dimantha <85834813+ashandimantha2000@users.noreply.github.com>
2023-02-24 11:38:40 +0530

    AdminSignIn

---
+++ adminsignin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/signIn.css">
+    <title>Admin SignIn - Tabsule</title>
+</head>
+<body>
+<body>
+    <div class="adminbg">
+        <div class="sign-in">
+            <div class="sign-in-form">
+                <h1>Sign In</h1>
+                <form action="index.php" method="POST">
+                    <input type="text" name="email" placeholder="Email" required>
+                    <br>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <br>
+                    <input type="submit" value="Sign In" class="sign-in-btn">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</body>
+</html>
