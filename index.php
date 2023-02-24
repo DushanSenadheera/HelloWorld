05c9951d148023a6fe802e3a3dc2ddc7650be0b7
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-02-24 11:10:06 +0530

    added home page

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Dushan</h1>
-    <h1>Ashan</h1>
-    <h1>Dimantha</h1>
-    <h1>Herath</h1>
+    <nav>
+        <h3>LOGO</h3>
+        <form action="" method="post">
+            <input class="searchParmacy" placeholder="Search by parmacy..." type="search" name="search" id="search">
+        </form>
+        <a href="">Show All Parmacies</a>
+    </nav>
+    <header>
+        <form action="" method="post">
+            <div>
+                <select name="" id="" class="dropdown">
+                    <option value="All">All</option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+                <input type="search" name="" id="" class="medSearch">
+                <input type="submit" value="Search">
+            </div>
+        </form>
+    </header>
 </body>
+
 </html>
